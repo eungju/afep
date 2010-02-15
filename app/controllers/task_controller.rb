757d98e0891fb45832fd4f568ef16c87ab31040c
Eung-ju PARK <eungju@gmail.com>
2010-02-16 00:41:25 +0900

    Task canceling.

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -20,6 +20,12 @@ class TaskController < ApplicationController
     task.save!
     redirect_to :action=>:index, :anchor => "task_#{task.id}"
   end
+  def cancel
+    task = Task.find(params[:id].to_i)
+    task.mark_as_canceled
+    task.save!
+    redirect_to :action=>:index, :anchor => "task_#{task.id}"
+  end
   def close_list
     uncompleted_task = CompletedList.new.find_uncompleted_task
     if uncompleted_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ActiveRecord::Base
   def mark_as_started
     if started? or completed?
-      raise "Already started"
+      raise "started already"
     end
     self.started_at = Time.now
   end
@@ -10,12 +10,21 @@ class Task < ActiveRecord::Base
   end
   def mark_as_completed
     if not started? or completed?
-      raise "Not started or already completed"
+      raise "Not started or completed already"
     end
     self.completed_at = Time.now
   end
   def completed?
     self.completed_at != nil
+  end
+  def mark_as_canceled
+    if started? or completed?
+      raise "started or completed already"
+    end
+    self.completed_at = Time.now
+  end  
+  def canceled?
+    self.started_at == nil and self.completed_at != nil
   end
   def self.find_uncompleted_before(closed_mark)
     Task.find(:first, :conditions=>["id <= ? and completed_at is null", closed_mark.task_id], :order=>"id", :limit=>1)
