272321dd9912a62fa3fd6bde469e5707cf713b20
Eung-ju PARK <eungju@gmail.com>
2010-04-29 17:38:29 +0900

    Remove RPX authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "task"
-  map.connect 'home', :controller => "home"
    
   # The priority is based upon order of creation: first created -> highest priority.
 
