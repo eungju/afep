d6a7b3f75e9051448794a7ab1dab38d8251c7396
Eung-ju PARK <eungju@gmail.com>
2010-09-24 16:07:48 +0900

    Update rails to 3.0.0.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_autofocus_session',
-  :secret      => '49fdb852b898cd5d047654b37301c28cb2507b7fb0e84ff5555f04baf0a2c0c13c4ddb642ad6966c942d8048c3bc52fff32fb6b7bc8972898e82dce869f574bd'
-}
+Autofocus::Application.config.session_store :cookie_store, :key => '_autofocus_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Autofocus::Application.config.session_store :active_record_store
