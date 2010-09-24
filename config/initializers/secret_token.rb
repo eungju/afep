efba5a787a4cb1ea8d4a54f9f5c6346f20738aa0
Eung-ju PARK <eungju@gmail.com>
2010-09-24 16:09:03 +0900

    Update rails to 3.0.0.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autofocus::Application.config.secret_token = '39f52bb2e2344de1e3537bd94a57ccfe18ef46fd648bbbc28dc822951b3d940bb9a401ff61b65f859448afa1caefbe47c95f8672c360e9c1648e94caaf1f99dc'
